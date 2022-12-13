982de76a01161af27d834c51a90cd1bd76701e12
Kesavan <94778213+kessvan@users.noreply.github.com>
2022-12-13 10:54:57 +0530

    Add files via upload

---
+++ Num.java
@@ -0,0 +1,20 @@
+class Num
+{
+// divisior number in java
+public static void main(String args[])
+{
+   int no=2;
+   int div=3;
+   int a=1;
+  while(a<=50)
+{
+  if(a%no==0&&a%div==0)
+{
+  System.out.println(a);
+}
+a++;
+}
+
+}
+
+}
