14f91eddd16dec0b3b807d278ee7aa33468e791e
Kesavan <94778213+kessvan@users.noreply.github.com>
2022-12-13 10:55:44 +0530

    Add files via upload

---
+++ Triangle.java
@@ -0,0 +1,21 @@
+class Triangle
+{
+public static void main(String []args)
+{
+for(int i=3;i>=1;i--)
+{
+for(int j=3;j>i;j--)
+{
+   System.out.print(" ");
+}
+for(int k=1;k<=i;k++)
+{
+System.out.print("*");
+
+}
+System.out.println("");
+
+}
+
+}
+}
